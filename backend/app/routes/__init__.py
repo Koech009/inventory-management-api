cd5ce764bad3d95d36faf1685c06352f4ded6144
Solomon Kirumba <solomonkirumbag@gmail.com>
2026-04-23 14:22:59 +0300

    add RBAC

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,13 +1,5 @@
-# app/routes/__init__.py
-from flask import Blueprint
-
 from .auth_routes import auth_bp
 from .product_routes import product_bp
 from .inventory_routes import inventory_bp
-# other imports...
-
-def register_routes(app):
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(product_bp)
-    app.register_blueprint(inventory_bp)
-    # register others...
+from .category_routes import category_bp
+from .supplier_routes import supplier_bp
